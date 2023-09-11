53cf212c95ae6a6dea15c07f60ba857c7df68c26
super <evanarendra7@gmail.com>
2023-09-12 01:06:24 +0700

    zupper 2

--- zupper/lib/main.dart
+++ zupper/lib/main.dart
@@ -4,10 +4,19 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  
-  Widget build(BuildContext context){
-
-  return MaterialApp(home:Center(child:Text('Hello There'),), );
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyanAccent[50],
+        appBar: AppBar(
+          title: Text('Zupper'),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: Center(
+          child: Image(image: AssetImage('images/ecommerce-plugins.jpg'),),
+        ),
+      ),
+    );
   }
 }
