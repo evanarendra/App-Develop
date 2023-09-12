7e494721b289071309e6d374e53310153af28134
super <evanarendra7@gmail.com>
2023-09-12 23:34:02 +0700

    button

--- zupper/lib/main.dart
+++ zupper/lib/main.dart
@@ -5,18 +5,31 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+  void selectAnswer(){
+    print('Answer Select');
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.cyanAccent[50],
-        appBar: AppBar(
-          title: Text('Zupper'),
-          backgroundColor: Colors.lightBlue,
-        ),
-        body: Center(
-          child: Image(image: AssetImage('images/ecommerce-plugins.jpg'),),
-        ),
-      ),
+          backgroundColor: Colors.cyanAccent[50],
+          appBar: AppBar(
+            title: Text('Zupper'),
+            backgroundColor: Colors.lightBlue,
+          ),
+          body: Column(
+            children: [
+              Text('Question'),
+              ElevatedButton(child: Text('Answer 1'), onPressed: ()=>print('Answer 1 Selected')),
+              ElevatedButton(child: Text('Answer 1'), onPressed: (){
+                //-------
+                print('Answer 2 Selected');
+              }),
+              ElevatedButton(child: Text('Answer 1'), onPressed: selectAnswer),
+              ElevatedButton(onPressed: selectAnswer, child: Text('Answer 1')),
+            ],
+          )),
     );
   }
 }
