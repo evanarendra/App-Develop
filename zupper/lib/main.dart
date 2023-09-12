3d846f772b89dfcd780ac5e9f306fb1d6516d34d
super <evanarendra7@gmail.com>
2023-09-12 23:38:52 +0700

    question 1

--- zupper/lib/main.dart
+++ zupper/lib/main.dart
@@ -5,12 +5,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
-  void selectAnswer(){
+  void selectAnswer() {
     print('Answer Select');
   }
 
   Widget build(BuildContext context) {
+    var question = [
+      'What is your favourite Game?',
+      'What is your favourite movies?'
+    ];
     return MaterialApp(
       home: Scaffold(
           backgroundColor: Colors.cyanAccent[50],
@@ -20,12 +23,16 @@ class MyApp extends StatelessWidget {
           ),
           body: Column(
             children: [
-              Text('Question'),
-              ElevatedButton(child: Text('Answer 1'), onPressed: ()=>print('Answer 1 Selected')),
-              ElevatedButton(child: Text('Answer 1'), onPressed: (){
-                //-------
-                print('Answer 2 Selected');
-              }),
+              Text(question.elementAt(0)),
+              ElevatedButton(
+                  child: Text('Answer 1'),
+                  onPressed: () => print('Answer 1 Selected')),
+              ElevatedButton(
+                  child: Text('Answer 1'),
+                  onPressed: () {
+                    //-------
+                    print('Answer 2 Selected');
+                  }),
               ElevatedButton(child: Text('Answer 1'), onPressed: selectAnswer),
               ElevatedButton(onPressed: selectAnswer, child: Text('Answer 1')),
             ],
